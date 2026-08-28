1c834e472fbfdb4f592e40db714b32b52acc25f7
bahez masoud <bahezmasoud@gmail.com>
2026-08-29 01:19:20 +0300

    time to pandas with csv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-array=[2,4,6]*2
-print(array)
-array1=np.array(array)*2
-print(array1)
+import pandas as pd
+df = pd.read_csv(r'C:\Users\bahez\OneDrive\Desktop\pyWithBara\organizations-100.csv',header=None,names=['orga','late'],usecols=[0,4],na_values=['?'],nrows=25,skiprows=25)
+df.isna().sum()
+df.dropna(inplace=True)
+print(df)
+help(pd.read_csv)
